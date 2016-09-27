af95cabc512f509e2b8d65307b78d13d5f4659f5
xubaifuCode <1660866966@qq.com>
2016-09-27 21:57:53 +0800

    完成刮刮卡基本功能

---
+++ js.js
@@ -0,0 +1,35 @@
+var flag = false;
+
+function init() {
+	var result = new Array("THANKS", "SUPER GOLD", "GIRL FIREND");
+	var mask = document.getElementById("mask");
+	var res = document.getElementById("result");
+
+	mask.style.left = res.offsetLeft + "px";
+	mask.style.top = res.offsetTop + "px";
+	mask.width = res.offsetWidth;
+	mask.height = res.offsetHeight;
+	res.innerHTML = result[Math.round(Math.random() * (result.length - 1))];
+
+	var ctx=mask.getContext('2d');
+	ctx.fillStyle = "gray";
+	ctx.fillRect(0,0,res.offsetWidth,res.offsetHeight);
+
+	mask.addEventListener("mousemove", mouseMove);
+	mask.addEventListener("mousedown", mouseDown);
+	mask.addEventListener("mouseup", mouseUp);
+
+	function mouseDown() {
+		flag = true;
+	}
+
+	function mouseUp() {
+		flag = false;
+	}
+
+	function mouseMove(e) {
+		if (flag) {
+			ctx.clearRect(e.clientX - res.offsetLeft, e.clientY - res.offsetTop, 10, 10);
+		}
+	}
+}
